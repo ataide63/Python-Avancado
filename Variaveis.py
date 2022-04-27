339f8aae1f10367bb7b83f9db011cabebbbdc8e2
Ataide <101526929+ataide63@users.noreply.github.com>
2022-04-27 16:22:17 -0300

    Add files via upload

---
+++ Variaveis.py
@@ -0,0 +1,12 @@
+
+# tipos de variáveis no Python
+__author__ = 'Sou Tata'
+print (__author__)
+
+__var_String__ = 'Sou String'
+print (__var_String__)
+
+__var_Num__ = 100
+print(f'Meu valor é, {__var_Num__}')
+a = 1 + 1
+print  (a)
---
+++ Visualiza_Grafico.py
@@ -0,0 +1,10 @@
+
+
+class Visualiza_data:
+    import Conexao_db
+    import matplotlib.pyplot as plt  #  projeto\site-package\matplotlib
+    import tkinter
+
+    x = []
+    y = []
+    plt.plot(x,y)
